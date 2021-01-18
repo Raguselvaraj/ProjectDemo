d747ca8672bfe94489976045da6f389470bc48dc
Ragu <mailmeragu92@gmail.com>
2021-01-18 16:47:24 +0530

    DONE KAARO

--- src/test/java/com/selenium/Day1.java
+++ src/test/java/com/selenium/Day1.java
@@ -2,6 +2,8 @@ package com.selenium;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class Day1 {
 	
@@ -12,6 +14,10 @@ public class Day1 {
 		driver.get("https://www.facebook.com/");
 		driver.manage().window().maximize();
 		
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\acer\\eclipse-workspace\\SeleniumTask\\Drivers\\chromedriver.exe");
+		WebDriver driver1=new InternetExplorerDriver();
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
 		
 	}
 
